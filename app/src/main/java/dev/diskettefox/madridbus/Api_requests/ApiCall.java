e410017a3881da3241a97ae2e5cd0441df0a0bdd
piero2607 <pieromanuelyovera@gmail.com>
2026-02-12 13:22:56 +0100

    ¨Added Api Requests modules

--- app/src/main/java/dev/diskettefox/madridbus/Api_requests/ApiCall.java
+++ app/src/main/java/dev/diskettefox/madridbus/Api_requests/ApiCall.java
@@ -1,7 +1,28 @@
 package dev.diskettefox.madridbus.Api_requests;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiCall {
-    public static String BASE_URL="";
+    public static String BASE_URL="https://datos.emtmadrid.es";
+    private static Retrofit retrofit;
 
+    public static Retrofit getStop(){
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor=new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+            OkHttpClient stop=new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build();
+            retrofit =new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(stop)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
