e1ce9d40470f7fb882487a5f0a09a791e5b94139
Yo fp <cosa@correo.com>
2026-02-12 14:05:59 +0100

    na raro

---
+++ app/src/main/java/dev/diskettefox/madridbus/Api_requests/ApiInterface.java
@@ -0,0 +1,14 @@
+package dev.diskettefox.madridbus.Api_requests;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface{
+
+    /*@GET("endpoint") ejemplos de busqueda
+    Call<Elemento> getElementos();
+    @GET("endpoint")
+    Call<Elemento>getElementoByName(@Query("filter[text]") String nombre);*/
+
+}
