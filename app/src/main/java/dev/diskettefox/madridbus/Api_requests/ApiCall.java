883b44b72dfb40e0960f24a2017ebb59dbc5f0e0
piero2607 <pieromanuelyovera@gmail.com>
2026-02-13 11:29:44 +0100

    agregar intento token fijo en clase apiCall

--- app/src/main/java/dev/diskettefox/madridbus/Api_requests/ApiCall.java
+++ app/src/main/java/dev/diskettefox/madridbus/Api_requests/ApiCall.java
@@ -1,12 +1,14 @@
 package dev.diskettefox.madridbus.Api_requests;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiCall {
     public static String BASE_URL="https://datos.emtmadrid.es";
+    public static final String token="4f6e5f2f-2f96-4415-b634-78ec45d10753";
     private static Retrofit retrofit;
 
     public static Retrofit getStop(){
@@ -16,6 +18,15 @@ public class ApiCall {
 
             OkHttpClient stop=new OkHttpClient.Builder()
                     .addInterceptor(interceptor)
+                    .addInterceptor(chain -> {
+                        Request request=chain.request().newBuilder()
+                                .addHeader(
+                                        "Authorization",
+                                        "Beader " +token
+                                )
+                                .build();
+                        return chain.proceed(request);
+                    })
                     .build();
             retrofit =new Retrofit.Builder()
                     .baseUrl(BASE_URL)
