56b72a66dc6f9dabb0a3aeb3c913f562cb94724a
piero2607 <pieromanuelyovera@gmail.com>
2026-02-17 10:39:39 +0100

    agregando imagenes de mapa1 y 2 en drawable

--- app/src/main/java/dev/diskettefox/madridbus/FragmentMap.java
+++ app/src/main/java/dev/diskettefox/madridbus/FragmentMap.java
@@ -6,12 +6,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+
+
 public class FragmentMap extends Fragment {
         public FragmentMap(){
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.view_map,container,false);
-    }
+            View view=inflater.inflate(R.layout.view_map,container,false);
+            ImageView imagenMap=view.findViewById(R.id.imgMap);
+            String url="https://www.crtm.es/media/t3wadujx/serie_2a1_madrid_ciudad_es.pdf";
+            Glide.with(requireContext())
+                    .load(url)
+                    .into(imagenMap);
+            return view;
+        }
 }
