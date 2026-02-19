749d50e292b4c66e168c4818c7ed1e84f85ec027
DisketteFox <diskettefox@outlook.com>
2026-02-19 21:32:02 +0100

    Search stop from number implemented

--- app/src/main/java/dev/diskettefox/madridbus/fragments/FragmentStop.java
+++ app/src/main/java/dev/diskettefox/madridbus/fragments/FragmentStop.java
@@ -1,13 +1,17 @@
 package dev.diskettefox.madridbus.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 
 import dev.diskettefox.madridbus.R;
+import dev.diskettefox.madridbus.StopActivity;
 import dev.diskettefox.madridbus.adapters.StopAdapter;
 import dev.diskettefox.madridbus.api.ApiCall;
 import dev.diskettefox.madridbus.api.ApiInterface;
@@ -36,7 +41,7 @@ import retrofit2.Response;
 
 public class FragmentStop extends Fragment {
     private final ArrayList<StopModel.Stop> stopsList = new ArrayList<>();
-    private final ArrayList<StopModel.Stop> filtroStops = new ArrayList<>();
+    private final ArrayList<StopModel.Stop> stopFilter = new ArrayList<>();
     private StopAdapter adapter;
     private LoadingIndicator loadingIndicator;
 
@@ -74,38 +79,51 @@ public class FragmentStop extends Fragment {
             }
         }
 
-        // ingerto de barra de busqueda X2.
-        SearchBar searchBar=view.findViewById(R.id.search_bar_Stops);
-        EditText editText=view.findViewById(R.id.stopsET);
-        // prueba inestable para que de la sensación de click
-        editText.setOnClickListener(new View.OnClickListener() {
+        // Search bar
+        SearchBar searchBar = view.findViewById(R.id.search_bar_Stops);
+        EditText editText = view.findViewById(R.id.stopsET);
+
+        // Handle Search action on keyboard
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                searchBar.setText(" ");
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE) {
+                    int searchStopId = Integer.parseInt(v.getText().toString());
+                    Log.d("Search", "Search button clicked for stop ID: " + searchStopId);
+                    if (searchStopId < 51182) {
+                        Intent intent = new Intent(getContext(), StopActivity.class);
+                        intent.putExtra("stopId", String.valueOf(searchStopId));
+                        getContext().startActivity(intent);
+                    } else {
+                        Log.d("Search Error", "Invalid stop ID: " + searchStopId);
+                    }
+                    return true;
+                }
+                return false;
             }
         });
-        // logica del "filtrado"
+
+        // Filter logic
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {}
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {filtroStops.clear();}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {stopFilter.clear();}
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 searchBar.setText(s);
 
-                for (StopModel.Stop parada: stopsList){
-                    if (parada.getStopId().contains(s)){
-                        filtroStops.add(parada);
+                for (StopModel.Stop stop: stopsList){
+                    if (stop.getStopId().contains(s)){
+                        stopFilter.add(stop);
                     }
                 }
                 recyclerStops.setLayoutManager(new LinearLayoutManager(getContext()));
-                adapter = new StopAdapter(getContext(), filtroStops);
+                adapter = new StopAdapter(getContext(), stopFilter);
                 recyclerStops.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
             }
         });
-
         return view;
     }
 
