0bf4cf08b40aff0361a584fa584e6558d7fb65b1
DisketteFox <diskettefox@outlook.com>
2026-02-19 08:48:28 +0100

    LineActivityAdaoter half implemented

--- app/src/main/java/dev/diskettefox/madridbus/LineActivity.java
+++ app/src/main/java/dev/diskettefox/madridbus/LineActivity.java
@@ -10,12 +10,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dev.diskettefox.madridbus.adapters.BusTimeAdapter;
+import dev.diskettefox.madridbus.adapters.LineActivityAdapter;
 import dev.diskettefox.madridbus.adapters.LineAdapter;
 import dev.diskettefox.madridbus.api.ApiCall;
 import dev.diskettefox.madridbus.api.ApiInterface;
@@ -26,16 +26,15 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LineActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private LineAdapter adapter;
-    private List<LineModel.Data> data = new ArrayList<>();
-    private TextView tvLineLabel, tvLineNameA, tvLineNameB;
+    private LineActivityAdapter adapter;
+    private final ArrayList<LineModel.Data> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_line);
+        ApiInterface apiInterface = ApiCall.callApi().create(ApiInterface.class);
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
@@ -47,49 +46,42 @@ public class LineActivity extends AppCompatActivity {
         int lineId = getIntent().getIntExtra("lineId", -1);
         Log.e("LineActivity", "Line ID: " + lineId);
 
-        tvLineLabel = findViewById(R.id.line_label);
-        tvLineNameA = findViewById(R.id.line_name_a);
-        tvLineNameB = findViewById(R.id.line_name_b);
+        // Initialize RecyclerView
+        adapter = new LineActivityAdapter(this, data);
 
         if (lineId != -1) {
-            tvLineLabel.setText(String.valueOf(lineId));
+            try {
+                fetchStopDetails(apiInterface, lineId);
+            } catch (NumberFormatException e) {
+                Log.e("StopActivity", "Invalid stop ID format", e);
+            }
         }
-
-
     }
 
-    private void fetchStopDetails(int lineId) {
-        ApiInterface apiInterface = ApiCall.callApi().create(ApiInterface.class);
+    private void fetchStopDetails(ApiInterface apiInterface, int lineId) {
         Call<LineModel> call = apiInterface.getLineDetail(lineId, ApiCall.token);
-
-        call.enqueue(new Callback<LineModel>() {
+        call.enqueue(new Callback<>() {
             @Override
-            public void onResponse(Call<LineModel> call, Response<LineModel> response) {
+            public void onResponse(@NonNull Call<LineModel> call, @NonNull Response<LineModel> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    LineModel lineModel = response.body();
-                    if (lineModel.getData() != null && !lineModel.getData().isEmpty()) {
-                        ArrayList<LineModel.Data> linesList = lineModel.getData();
+                    LineModel line = response.body();
 
-                        // Update UI with stop details from API
-                        if (data.get(0).getLabel() != null) {
-                            tvLineLabel.setText(data.get(0).getLabel());
-                        }
-                        if (data.get(0).getNameA() != null) {
-                            tvLineNameA.setText(data.get(0).getNameA());
-                        }
-                        if (data.get(0).getNameB() != null) {
-                            tvLineNameA.setText(data.get(0).getNameB());
-                        }
-
-                        linesList.clear();
+                    // Get data
+                    if (line.getData() != null) {
+                        data.addAll(line.getData());
                         adapter.notifyDataSetChanged();
+                        Log.d("JustWorking", "Line loaded");
+                    } else {
+                        Log.d("API Response", "No stops data for line ID: " + lineId);
                     }
+                } else {
+                    Log.d("API Response", "Failed response for line ID: " + lineId + ", Response: " + response);
                 }
             }
 
             @Override
             public void onFailure(Call<LineModel> call, Throwable t) {
-                Log.e("StopActivity", "Error fetching stop details", t);
+                Log.e("StopActivity", "Error fetching line details", t);
             }
         });
         }
