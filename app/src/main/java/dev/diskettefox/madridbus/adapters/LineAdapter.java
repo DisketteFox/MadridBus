3524df74519d7419f686d101552b8e73c072bcd2
Yo_Fp <yo@correo.com>
2026-02-19 20:35:06 +0100

    Añadido la barra de busqueda a las paradas y cambio de nombre de una ID. buenas noches

--- app/src/main/java/dev/diskettefox/madridbus/adapters/LineAdapter.java
+++ app/src/main/java/dev/diskettefox/madridbus/adapters/LineAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +16,6 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 import dev.diskettefox.madridbus.LineActivity;
 import dev.diskettefox.madridbus.R;
