76fb0396ef403402ac6a58a73bd750ea58363eaf
FP25 <estebitan2612@gmail.com>
2026-02-20 08:23:18 +0100

    s

--- app/src/main/java/dev/diskettefox/madridbus/FragmentLines.java
+++ app/src/main/java/dev/diskettefox/madridbus/FragmentLines.java
@@ -1,11 +1,26 @@
 package dev.diskettefox.madridbus;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.diskettefox.madridbus.adapters.BusAdapter;
+import dev.diskettefox.madridbus.api.ApiCall;
+import dev.diskettefox.madridbus.api.ApiInterface;
+import dev.diskettefox.madridbus.api.StopModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FragmentLines extends Fragment {
     public FragmentLines(){
@@ -13,6 +28,10 @@ public class FragmentLines extends Fragment {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.view_lines, container, false);
+        View view= inflater.inflate(R.layout.view_lines, container, false);
+
+        
+
+        return view;
     }
 }
