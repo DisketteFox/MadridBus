d092de8724eb2b7e65982264a6a7df389af5cf03
DisketteFox <diskettefox@outlook.com>
2026-02-22 23:30:23 +0100

    Added no favorites and half implemented no connection

--- app/src/main/java/dev/diskettefox/madridbus/fragments/FragmentStop.java
+++ app/src/main/java/dev/diskettefox/madridbus/fragments/FragmentStop.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,10 +48,10 @@ import retrofit2.Response;
 
 public class FragmentStop extends Fragment {
     private final ArrayList<StopModel.Stop> stopsList = new ArrayList<>();
-    private final ArrayList<StopModel.Stop> stopFilter = new ArrayList<>();
     private final ArrayList<BaseDatosModel> favorites = new ArrayList<>();
     private StopAdapter adapter;
     private LoadingIndicator loadingIndicator;
+    private LinearLayout noFavorites, noConnection;
 
     // private final int[] stopIds = {};
     private final int[] stopIds = {5710, 3862, 3542, 4812, 666};
@@ -62,6 +63,8 @@ public class FragmentStop extends Fragment {
         View view = inflater.inflate(R.layout.view_stops, container, false);
         RecyclerView recyclerStops = view.findViewById(R.id.recycler_stops);
         loadingIndicator = view.findViewById(R.id.progress_bar);
+        noFavorites = view.findViewById(R.id.no_favorites);
+        noConnection = view.findViewById(R.id.no_connection);
 
         ApiInterface apiInterface = ApiCall.callApi().create(ApiInterface.class);
         String accessToken = ApiCall.token;
@@ -75,7 +78,7 @@ public class FragmentStop extends Fragment {
         if (stopIds.length != 0) {
             loadingIndicator.setVisibility(View.VISIBLE);
         } else {
-            // No favorites image
+            noFavorites.setVisibility(View.VISIBLE);
         }
 
         // Populate the map for sorting
@@ -125,26 +128,6 @@ public class FragmentStop extends Fragment {
             }
         });
 
-        // Filter logic
-        // searchView.getEditText().addTextChangedListener(new TextWatcher() {
-        //     @Override
-        //     public void afterTextChanged(Editable s) {}
-        //     @Override
-        //     public void beforeTextChanged(CharSequence s, int start, int count, int after) {stopFilter.clear();}
-        //     @Override
-        //     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        //         searchBar.setText(s);
-        //         for (StopModel.Stop stop: stopsList){
-        //             if (stop.getStopId().contains(s)){
-        //                 stopFilter.add(stop);
-        //             }
-        //         }
-        //         recyclerStops.setLayoutManager(new LinearLayoutManager(getContext()));
-        //         adapter = new StopAdapter(getContext(), stopFilter);
-        //         recyclerStops.setAdapter(adapter);
-        //         adapter.notifyDataSetChanged();
-        //     }
-        // });
 
         getMyFavoritesStops();
 
@@ -213,6 +196,7 @@ public class FragmentStop extends Fragment {
             @Override
             public void onFailure(@NonNull Call<StopModel> call, @NonNull Throwable t) {
                 Log.e("Call Error", "Error retrieving data for stop ID: " + stopId, t);
+                noConnection.setVisibility(View.VISIBLE);
                 if (getContext() != null) {
                     Toast.makeText(getContext(), R.string.stop_error, Toast.LENGTH_SHORT).show();
                 }
@@ -224,7 +208,7 @@ public class FragmentStop extends Fragment {
     private void onResponseReceived() {
         responsesReceived++;
         if (responsesReceived == stopIds.length) {
-            // All responses received, now sort the list and update the UI
+            // Sorting list
             synchronized (stopsList) {
                 Collections.sort(stopsList, Comparator.comparing(stop -> stopIdToIndex.get(Integer.parseInt(stop.getStopId()))));
             }
